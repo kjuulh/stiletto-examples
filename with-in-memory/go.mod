cf6d2aecd0b1b5725d7159296f979b0b5e743fdd
kjuulh <contact@kjuulh.io>
2022-06-08 22:57:09 +0200

    Add first example

--- with-in-memory/go.mod
+++ with-in-memory/go.mod
@@ -1,3 +1,5 @@
 module github.com/kjuulh/stiletto-examples/with-in-memory
 
 go 1.18
+
+require github.com/kjuulh/stiletto v0.0.0-20220608205021-cdfbe9671e64 // indirect
--- with-in-memory/main.go
+++ with-in-memory/main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"github.com/kjuulh/stiletto/pkg/client"
+	"github.com/kjuulh/stiletto/pkg/featurestores"
+)
+
 func main() {
+
+	sClient := client.NewStilettoClient()
+
+	sClient.SetFeatureStore(
+		"userIDs",
+		featurestores.NewInMemoryFeatureStore([]string{"some-user-id"}))
+
+	featureActive, err := sClient.GetFeature(
+		context.Background(),
+		"userIDs",
+		"some-user-id")
+	if err != nil {
+		panic(err)
+	}
+
+	if featureActive {
+		fmt.Println("Feature is active, yay!")
+	}
 }
